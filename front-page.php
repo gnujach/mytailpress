688b20c3bbfd198943bf528e4f914edc00e7cbfa
gnujach <julian.abarca@gmail.com>
2021-12-22 10:08:05 -0600

    Agregamos enlace en front-page

--- front-page.php
+++ front-page.php
@@ -4,6 +4,11 @@
         <div class="max-w-full mx-auto">
             <img class="z-50" src="<?php echo get_bloginfo('template_url') . '/assets/images/centro-victoria.jpg' ?>" />
         </div>
+        <div class="max-w-sm my-8">
+            <a href="https://drive.google.com/file/d/1TkIgcsEwoOT9hFt8NdFIOwHv9V3fSNMy/view">
+                <img class="z-50" src="<?php echo get_bloginfo('template_url') . '/assets/images/convocatoria-controlaria.jpg' ?>" />
+            </a>
+        </div>
 
     <?php endif; ?>
 
