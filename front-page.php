021405f95a72ecfce2cbb62ef74b7223c59b0eab
Jose Julian Abarca <julian.abarca@gmail.com>
2022-07-07 15:46:05 -0500

    Actualizacion Portada

--- front-page.php
+++ front-page.php
@@ -9,6 +9,40 @@
                 <img src="https://victoriaguanajuato.gob.mx/wp-content/uploads/2022/07/feria-servicios.jpg" />
             </a>
         </div>
+        <div class="flex flex-col md:flex-row justify-between bg-green-500 text-white font-bold ml-2 mr-2">
+            <p class="pl-4 mx-2">Enlaces de Interes</p>
+        </div>
+        <div>
+            <section class="bg-white dark:bg-gray-900">
+                <div class="container px-6 py-12 mx-auto">
+                    <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                        <div>
+                            <a href="https://bit.ly/3OMNAZs">
+                                <img src="https://victoriaguanajuato.gob.mx/wp-content/uploads/2022/07/juventudes.jpeg" class="rounded-md">
+                            </a>
+                            <h1 class="mt-4 text-xl font-semibold text-gray-800 dark:text-white">JUVENTUDES GRANDEZA</h1>
+                            <p class="mt-2 text-gray-500 dark:text-gray-400">Hoy tú tienes la decisión para que tu voz se escuche.
+                                Si eres un joven visionario al que le interesa buscar soluciones a problemáticas sociales, participa en nuestra convocatoria.</p>
+                        </div>
+
+                        <div>
+                            <a href="https://juventudesgto.guanajuato.gob.mx/wp-content/uploads/Convocatoria-Talento-de-Exportacio%CC%81n-2.pdf">
+                                <img src="https://victoriaguanajuato.gob.mx/wp-content/uploads/2022/07/talento.jpeg" class="rounded-md">
+                            </a>
+                            <h1 class="mt-4 text-xl font-semibold text-gray-800 dark:text-white">Talento de Exportación</h1>
+                            <p class="mt-2 text-gray-500 dark:text-gray-400">Talento de Exportación.</p>
+                        </div>
+                        <div>
+                            <a href="https://fondos.guanajuato.gob.mx/fondos/crece-tu-negocio/">
+                                <img src="https://victoriaguanajuato.gob.mx/wp-content/uploads/2022/07/crea_negocio.jpeg" class="rounded-md">
+                            </a>
+                            <h1 class="mt-4 text-xl font-semibold text-gray-800 dark:text-white">Crece tu Negocio</h1>
+                            <p class="mt-2 text-gray-500 dark:text-gray-400">Crece tu Negocio</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
 
     <?php endif; ?>
 
