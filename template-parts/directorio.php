124e3af1bbbd6e8c147cd3ce2e45a66dc3cf0c99
Jose Julian Abarca <julian.abarca@gmail.com>
2022-07-07 21:11:20 -0500

    Creamos pagina de directorio

---
+++ template-parts/directorio.php
@@ -0,0 +1,86 @@
+<div class="flex flex-col md:flex-row justify-content m-2">
+    <article class="p-6 bg-white sm:p-8 rounded-xl ring ring-indigo-50 mx-2 my-2">
+        <div class="flex items-start flex-wrap">
+            <div class="hidden sm:grid sm:h-20 sm:w-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-blue-600" aria-hidden="true">
+                <div class="flex items-center gap-1">
+                    <img src="https://victoriaguanajuato.gob.mx/wp-content/uploads/2021/11/cropped-cropped-logo2021-2024-150x90-1.png">
+                </div>
+            </div>
+
+            <div class="sm:ml-8">
+                <h2 class="rounded border border-blue-600 bg-blue-500 px-3 py-1.5 mt-4 text-lg font-medium sm:text-xl text-white">
+                    Desarrollo Urbano y Obras Públicas
+                </h2>
+                <h3 class="mt-4 text-lg font-medium sm:text-xl">
+                    Arq. José Alvarado Briones
+                </h3>
+                <div class="flex items-start gap-1 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                    <p class="text-xl text-gray-700">
+                        j_alvarado@victoria.gob.mx
+                    </p>
+                </div>
+                <div class="flex items-start gap-1 md:h-8 sm:h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    <p class="text-xl text-gray-700">
+                        Callejón del Gallo S/N, Jardín Principal
+                    </p>
+                </div>
+                <div class="flex items-start gap-1 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                    </svg>
+                    <p class="text-xl text-gray-700">
+                        419.234.31.16
+                    </p>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="p-6 bg-white sm:p-8 rounded-xl ring ring-indigo-50 mx-2 my-2">
+        <div class="flex items-start">
+            <div class="hidden sm:grid sm:h-20 sm:w-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-blue-600" aria-hidden="true">
+                <div class="flex items-center gap-1">
+                    <img src="https://victoriaguanajuato.gob.mx/wp-content/uploads/2021/11/cropped-cropped-logo2021-2024-150x90-1.png">
+                </div>
+            </div>
+
+            <div class="sm:ml-8">
+                <h2 class="rounded border border-blue-600 bg-blue-500 px-3 py-1.5 mt-4 text-lg font-medium sm:text-xl text-white">
+                    Desarrollo Social y Humano
+                </h2>
+                <h3 class="mt-4 text-lg font-medium sm:text-xl">
+                    Lic. Luis Antonio Charre Verde
+                </h3>
+                <div class="flex items-start gap-1 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                    <p class="text-xl text-gray-700">
+                        l_charre@victoria.gob.mx
+                    </p>
+                </div>
+                <div class="flex items-start gap-1 h-10">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    <p class="sm:text-base md:text-xl text-gray-700">
+                        Callejón del Gallo S/N, Jardín Principal
+                    </p>
+                </div>
+                <div class="flex items-start gap-1 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                    </svg>
+                    <p class="text-xl text-gray-700">
+                        419.234.31.14
+                    </p>
+                </div>
+            </div>
+        </div>
+    </article>
+</div>
