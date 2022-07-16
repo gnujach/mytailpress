e7ba02dc03461274dccd57169c282148aeca5427
Jose Julian Abarca <julian.abarca@gmail.com>
2022-07-16 17:14:36 -0500

    Modiifamos el footer con el color de fondo solicitado

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 </div>
 
 <?php do_action('tailpress_content_after'); ?>
-<footer class="p-4 bg-gray-100 sm:p-6 min-w-full">
+<footer class="p-4 bg-blue-100 sm:p-6 min-w-full">
 	<div class="md:flex md:justify-between space-x-4">
 		<div class="mb-6 md:mb-0">
 			<a href="#" class="flex items-center flex-col">
@@ -15,17 +15,17 @@
 		</div>
 		<div class="grid grid-cols-2 grid-flow md:gap-8 sm:gap-6 sm:grid-cols-2">
 			<div>
-				<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">Información</h2>
-				<ul class="text-gray-600 dark:text-gray-400">
+				<h2 class="mb-6 text-sm text-blue-800 font-semibold uppercase ">Información</h2>
+				<ul class="text-blue-800  dark:text-gray-400">
 					<li class="mb-4">
 						<div class="flex items-center gap-2">
-							<span class="h-8">
+							<span class="h-8 text-blue-800">
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 									<path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
 									<path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 								</svg>
 							</span>
-							<p class="break-words ">Palacio Municipal S/N, Colonia Centro</p>
+							<p class="break-words text-blue-800 font-semibold">Palacio Municipal S/N, Colonia Centro</p>
 						</div>
 					</li>
 					<li>
@@ -33,7 +33,7 @@
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 								<path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
 							</svg>
-							<p>419.234.31.00</p>
+							<a href="tel:419.234.31.00">419.234.31.00</a>
 						</div>
 					</li>
 					<li>
@@ -49,10 +49,10 @@
 				</ul>
 			</div>
 			<div>
-				<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">Aviso de Privacidad</h2>
+				<h2 class="mb-6 text-sm text-blue-800 font-semibold uppercase ">Aviso de Privacidad</h2>
 				<ul class="text-gray-600 dark:text-gray-400">
 					<li class="mb-4">
-						<a href="#" class="hover:underline">Legal</a>
+						<a href="#" class="hover:underline text-blue-800 font-semibold">Legal</a>
 					</li>
 				</ul>
 			</div>
@@ -60,23 +60,26 @@
 	</div>
 	<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 	<div class="sm:flex sm:items-center sm:justify-between">
-		<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"><?php echo date_i18n('Y'); ?> <a href="#" class="hover:underline"><?php echo get_bloginfo('name'); ?></a>. Todos los derechos reservados.
+		<span class="text-sm text-blue-800 font-semibold sm:text-center dark:text-gray-400"><?php echo date_i18n('Y'); ?> <a href="#" class="hover:underline"><?php echo get_bloginfo('name'); ?></a>. Todos los derechos reservados.
 		</span>
 		<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-			<a href="https://www.facebook.com/AdministracionVictoriaGto" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+			<a href="https://www.facebook.com/AdministracionVictoriaGto" class="text-blue-800 font-semibold hover:text-gray-900 dark:hover:text-white">
 				<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 					<path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
 				</svg>
 			</a>
-			<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+			<a href="https://www.instagram.com/municipiodevictoriagto/" class="text-blue-800 font-semibold hover:text-gray-900 dark:hover:text-white">
 				<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 					<path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
 				</svg>
 			</a>
-			<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-				<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-					<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+
+			<a href="https://www.youtube.com/channel/UC6NG_nVUpBbRe-dVrHFhS6w" class="text-blue-800 font-semibold hover:text-gray-900 dark:hover:text-white">
+				<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M14 12L10.5 14V10L14 12Z" fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+					<path d="M2 12.7075V11.2924C2 8.39705 2 6.94939 2.90549 6.01792C3.81099 5.08645 5.23656 5.04613 8.08769 4.96549C9.43873 4.92728 10.8188 4.8999 12 4.8999C13.1812 4.8999 14.5613 4.92728 15.9123 4.96549C18.7634 5.04613 20.189 5.08645 21.0945 6.01792C22 6.94939 22 8.39705 22 11.2924V12.7075C22 15.6028 22 17.0505 21.0945 17.9819C20.189 18.9134 18.7635 18.9537 15.9124 19.0344C14.5613 19.0726 13.1812 19.1 12 19.1C10.8188 19.1 9.43867 19.0726 8.0876 19.0344C5.23651 18.9537 3.81097 18.9134 2.90548 17.9819C2 17.0505 2 15.6028 2 12.7075Z" stroke="currentColor" stroke-width="1.5" />
 				</svg>
+
 			</a>
 		</div>
 	</div>
